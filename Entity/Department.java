94609ce8c77f247374d5b731065f06d66d26056c
v12581234 <vinhnguyen125812@gmail.com>
2025-07-05 05:38:15 +0700

    Add Properties and Method Into Department Class

--- Entity/Department.java
+++ Entity/Department.java
@@ -1,5 +1,21 @@
 package Entity;
 
-public class Department {
-    
+public  class Manager extends Employee{
+    public Manager() {
+    }
+
+    public Manager(int id, String name, int age, String address, double baseSalary, String position, String departmentId,double salary) {
+        super(id, name, age, address, baseSalary, position, departmentId,salary);
+    }
+
+    @Override
+    public double calculateSalary(){
+        return (getBaseSalary() + 5000000);
+        
+    }
+    @Override
+public  Employee inputInformation(){
+    super.inputInformation();
+    return new Manager(getId(), getName(), getAge(), getAddress(), getBaseSalary(),"Manager", getDepartmentId(),calculateSalary());
+}
 }
