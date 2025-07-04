65965753e32cadb9bb8b235dfafe3775510b10c0
v12581234 <vinhnguyen125812@gmail.com>
2025-07-05 05:33:29 +0700

    Add Properties And Methods Into Entity Package

--- Entity/ViceManager.java
+++ Entity/ViceManager.java
@@ -1,5 +1,23 @@
 package Entity;
 
-public class ViceManager {
+public  class ViceManager extends  Employee{
+    public ViceManager() {
+    }
+
+    public ViceManager(int id, String name, int age, String address, double baseSalary, String position, String departmentId,double salary) {
+        super(id, name, age, address, baseSalary, position, departmentId,salary);
+    }
+
+    @Override
+    public double calculateSalary(){
+    return getBaseSalary()+3000000;
+    }
+
+    @Override
+    public  Employee inputInformation(){
+        super.inputInformation();
+        return new ViceManager(getId(), getName(), getAge(), getAddress(), getBaseSalary(),"ViceManager", getDepartmentId(),calculateSalary());
     
+
+}
 }
